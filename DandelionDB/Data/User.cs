fa95ee21a96635ac349cca2b9fe0d913bf6b8de5
Jsqadro <hristolubovasi@gmail.com>
2023-11-25 22:14:14 +0500

    DB
    Add DB folder

---
+++ DandelionDB/Data/User.cs
@@ -0,0 +1,10 @@
+﻿namespace mathsteam.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int PasswordHash { get; set; }
+    }
+}
