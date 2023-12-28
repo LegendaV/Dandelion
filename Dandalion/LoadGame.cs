56ba1d93f30bd6b240e92058f0fa7e8929b60634
LegendaV <115637903+LegendaV@users.noreply.github.com>
2023-12-28 23:13:06 +0500

    Add LoadGame

---
+++ Dandalion/LoadGame.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Net;
+
+namespace Dandalion
+{
+    public static class GameLoader
+    {
+        private static string outputFolder = "Games";
+        public static void Load(string repositoryUrl = "https://github.com/LegendaV/DiskUsage", string commitSha = "f916cab7e86043b471173d74648f155b25b757b7")
+        {
+            string downloadUrl = $"{repositoryUrl}/archive/{commitSha}.zip";
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(downloadUrl, "temp.zip");
+                ZipFile.ExtractToDirectory("temp.zip", Path.Combine(Directory.GetCurrentDirectory(), outputFolder));
+                File.Delete("temp.zip");
+            }
+        }
+    }
+}
